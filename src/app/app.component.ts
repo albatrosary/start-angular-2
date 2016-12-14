b7a969b7e14cce23b53ebdf742408f56cfd2cafe
Fumio SAGAWA <fumio.sagawa@gmail.com>
2016-12-14 17:10:17 +0900

    lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,25 @@ import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor (titleService:Title, router:Router, activatedRoute:ActivatedRoute) {
+  constructor (
+    titleService: Title,
+    router: Router,
+    activatedRoute: ActivatedRoute
+  ) {
     router.events.subscribe(event => {
-      if(event instanceof NavigationEnd) {
-        var title = this.getTitle(router.routerState, router.routerState.root).join('-');
+      if (event instanceof NavigationEnd) {
+        let title = this.getTitle(router.routerState, router.routerState.root).join('-');
         titleService.setTitle(title);
       }
     });
   }
   getTitle(state, parent) {
-    var data = [];
-    if(parent && parent.snapshot.data && parent.snapshot.data.title) {
+    let data = [];
+    if (parent && parent.snapshot.data && parent.snapshot.data.title) {
       data.push(parent.snapshot.data.title);
     }
 
-    if(state && parent) {
+    if (state && parent) {
       data.push(... this.getTitle(state, state.firstChild(parent)));
     }
     return data;
