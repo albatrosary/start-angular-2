3455508d475a2325ee17118049da8f2328e76e5f
Fumio SAGAWA <fumio.sagawa@gmail.com>
2016-12-14 17:05:02 +0900

    add module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,29 +1,24 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { IssueModule } from './issue/issue.module';
+import { WikiModule } from './wiki/wiki.module';
 
 import { PagesComponent } from './pages.component';
-import { WikiComponent } from './wiki/wiki.component';
 import { TopComponent } from './top/top.component';
-
-import { MarkdownPipe } from '../markdown.pipe';
 
 import { pagesRouting, pagesRoutingProviders }  from './pages.routes';
 
 @NgModule({
   imports: [
-    IssueModule,
     FormsModule,
-    BrowserModule,
-    pagesRouting
+    pagesRouting,
+    IssueModule,
+    WikiModule
   ],
   declarations: [
     PagesComponent,
-    WikiComponent,
-    TopComponent,
-    MarkdownPipe
+    TopComponent
   ],
   providers: [
     pagesRoutingProviders
---
+++ src/app/pages/wiki/wiki.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { CommonModule } from '@angular/common';
+import { WikiComponent } from './wiki.component';
+
+import { MarkdownPipe } from './markdown.pipe';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  declarations: [
+    WikiComponent,
+    MarkdownPipe
+  ]
+})
+export class WikiModule { }
